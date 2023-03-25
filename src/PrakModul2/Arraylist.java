ea837ef393e7a63b5b83dcbfac58d29383346c09
MiftahulPtr <miftahulptral@gmail.com>
2023-03-25 08:32:28 -0500

    first commit

---
+++ src/PrakModul2/Arraylist.java
@@ -0,0 +1,37 @@
+package PrakModul2;
+import java.util.LinkedList;
+
+public class Arraylist {
+	public static void main(String[] args) {
+		LinkedList<String> hewan = new LinkedList<>();
+		LinkedList<String> DeleteHewan = new LinkedList<>();
+		
+		//ArrayList <String> hewan = new ArrayList<>();
+		//ArrayList <String> DeleteHewan = new ArrayList<>();
+		
+		hewan.add("sapi");
+		hewan.add("kelinci");
+		hewan.add("kambing");
+		hewan.add("unta");
+		hewan.add("domba");
+		System.out.println("Hewan : "+hewan);
+		
+		DeleteHewan.add("kelinci");
+		DeleteHewan.add("kambing");
+		DeleteHewan.add("unta");
+		System.out.println("Hewan yang dihapus: ");
+		System.out.println(DeleteHewan);
+		
+		for(int i=0; i<hewan.size();i++) {
+			for(int j=0; j<DeleteHewan.size();j++) {
+				if(hewan.get(i) == DeleteHewan.get(j)) {
+					hewan.remove(i);
+				}
+			}
+		}
+		
+		System.out.println("Output hewan : ");
+		System.out.println(hewan);
+		
+	}
+}
